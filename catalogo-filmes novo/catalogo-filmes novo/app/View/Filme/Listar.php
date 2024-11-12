3de5de34f64a3703f50272eaf9bab5e13283972d
Fred-Curso-Do-Senac-2024-Tc-ADS <frederico5711126@aluno.ms.senac.br>
2024-11-11 21:21:03 -0400

    Add files via upload

---
+++ catalogo-filmes novo/catalogo-filmes novo/app/View/Filme/Listar.php
@@ -0,0 +1,82 @@
+<?php
+
+require __DIR__ . "\..\..\Config\Database.php";
+//require __DIR__ . "\..\..\Model\Filme.php";
+require __DIR__ . "\..\..\Model\Usuario.php";
+
+//$filmeModel = new Filme($conn);
+$filmeModel = new Usuario($conn);
+//$filmes = $filmeModel->findAll();
+$filmes = $usuarioModel->findAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuario</title>
+
+    <style>
+        .table {
+            tr, td, th {
+                border: 1px solid;
+            }
+        }
+    </style>
+</head>
+<body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Email</th>
+                <!--<th>Ano</th>-->
+                <th>Descrição</th>
+                <!--<th>Ação</th>--> <!-- -->
+            </tr>
+        </thead>
+        <tbody>
+            <!-- percorre a lista de resultados -->
+            <?php foreach ($filmes as $filme) { ?>
+                
+                
+                <tr>
+                    <!-- escreve na tabela cada item retornado -->
+                    <td><?php echo $filme->id ?></td>
+                    <td><?php echo $filme->titulo ?></td>
+                    <td><?php echo $filme->ano ?></td>
+                    <td><?php echo $filme->descricao ?></td>
+                    <td>
+                        <form action="Visualizar.php" method="GET">
+                            <input 
+                                type="hidden"
+                                name="id" 
+                                value="<?php echo $filme->id ?>"
+                            >
+                            <button>Detalhes</button>
+                        </form>
+
+                        <form action="Excluirr.php" method="POST">
+                            <input 
+                                type="hidden"
+                                name="id" 
+                                value="<?php echo $filme->id ?>"
+                            >
+                            <button>Excluir</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+
+    <script>
+        /* se o php retornar mensagem -> Erro ao Exclui
+        se o php retornar mensagem -> sucesso -> o filme foi Excluido*/ 
+
+        document.getElementById('mensagem').innerHTML = '<h1>RRO</H1>'
+    </script>
+</body>
+</html>
